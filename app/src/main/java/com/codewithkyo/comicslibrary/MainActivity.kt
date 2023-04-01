3633e868733d83f8821664c95c311edd2f5bfa4b
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-04-01 10:41:43 +0900

    Compose Navigation

--- app/src/main/java/com/codewithkyo/comicslibrary/MainActivity.kt
+++ app/src/main/java/com/codewithkyo/comicslibrary/MainActivity.kt
@@ -5,12 +5,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.codewithkyo.comicslibrary.ui.theme.ComicsLibraryTheme
+import com.codewithkyo.comicslibrary.view.CharactersBottomNav
+import com.codewithkyo.comicslibrary.view.CollectionScreen
+import com.codewithkyo.comicslibrary.view.LibraryScreen
+
+sealed class Destination(val route: String) {
+    object Library: Destination("library")
+    object Collection: Destination("collection")
+    object CharacterDetail: Destination("character/{characterId}") {
+        fun createRoute(characterId: Int?) = "character/$characterId"
+    }
+}
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +38,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    CharacterScaffold(navController = navController)
                 }
             }
         }
@@ -30,14 +47,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun CharacterScaffold(navController: NavHostController) {
+    val scaffoldState = rememberScaffoldState()
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComicsLibraryTheme {
-        Greeting("Android")
+    Scaffold(
+        scaffoldState = scaffoldState,
+        bottomBar = { CharactersBottomNav(navController = navController)}
+    ) { paddingValues ->
+        NavHost(navController = navController, startDestination = Destination.Library.route) {
+            composable(Destination.Library.route) {
+                LibraryScreen()
+            }
+            composable(Destination.Collection.route) {
+                CollectionScreen()
+            }
+            composable(Destination.CharacterDetail.route) { navBackStackEntry ->
+
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/codewithkyo/comicslibrary/view/CharacterDetailScreen.kt
@@ -0,0 +1,9 @@
+package com.codewithkyo.comicslibrary.view
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CharacterDetailScreen() {
+    Text("Character Detail screen")
+}
